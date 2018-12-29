fc2d7a9aca0fa7775da0717a67e76943c9d8c048
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-29 21:12:42 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   def index
     @products = cart
-    byebug
   end
 
   def add
     session[:cart] << params[:product]
+    byebug
     redirect_to root_path
   end
 end
